5938aa6eada77e03d57f48e25790acb00ec369dc
Alexey Fedoseev <aleksey@fedoseev.net>
2026-08-06 10:38:16 +0300

    turtle launch file

---
+++ launch/start.launch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    turtle_node = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        name='turtlesim_node'
+    )
+    driver_node = Node(
+        package='turtle_driver',
+        executable='driver',
+        name='driver'
+    )
+    return LaunchDescription([
+        turtle_node, driver_node
+    ])
